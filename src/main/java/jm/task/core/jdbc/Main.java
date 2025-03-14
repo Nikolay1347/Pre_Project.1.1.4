81f41830a51d3e4d1491969c6ae3477a72bad50c
Nikolay1347 <kolyan29041985@gmail.com>
2025-03-14 15:22:45 +0300

    задачу выполнил

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,20 +7,20 @@ import jm.task.core.jdbc.util.Util;
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        Util.getConnection();
-
+    public static void main(String[] args) throws SQLException {
         UserDao userDao = new UserDaoJDBCImpl();
-
+        userDao.createUsersTable();
 
         userDao.saveUser("Иван", "Соколов", (byte) 20);
         userDao.saveUser("Артём", "Федоров", (byte) 24);
         userDao.saveUser("Яна", "Иванова", (byte) 32);
         userDao.saveUser("Милана", "Князева", (byte) 27);
 
-        userDao.removeUserById(2);
-        userDao.getAllUsers();
+        System.out.println(userDao.getAllUsers());
+
         userDao.cleanUsersTable();
+
+        userDao.dropUsersTable();
 
     }
 }
